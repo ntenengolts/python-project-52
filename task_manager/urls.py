63cc3ec49f845a112afa2220e691f94fbc5fe64a
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-06-13 23:29:19 +0300

    Fix7(urls) Step 1

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.https import HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
