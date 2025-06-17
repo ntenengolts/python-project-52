878dae780741fbc82a572da405cf9ce3106ad5be
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-06-17 22:24:13 +0300

    Fix4 Step 4

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from task_manager.users.views import CostomLogoutView
+from task_manager.users.views import CustomLogoutView
 
 
 urlpatterns = [
@@ -26,6 +26,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('users/', include('task_manager.users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', CostomLogoutView.as_view(), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('statuses/', include('task_manager.statuses.urls')),
 ]
