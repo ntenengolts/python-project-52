131a42c12fad12dde135d57f1313f62a58b3d015
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-06-18 01:13:43 +0300

    Fix2 Step 5

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
 ]
