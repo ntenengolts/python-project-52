000c07b1964ba6ec308cba870cccf4335ddc3914
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-07-08 01:01:11 +0300

    fix18 text

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        full_name = self.get_full_name()
+        return full_name if full_name else self.username
 
     class Meta:
         verbose_name = "Пользователь"
