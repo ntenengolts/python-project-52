6b688cc4ccc47443b0dcfe1af165cc96fa0fe435
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-07-09 00:09:25 +0300

    fix22 final

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -46,7 +46,7 @@ class UserDeleteView(SuccessMessageMixin, DeleteView):
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
-        return super().delete(request, *args, **kwargs)
+        return super().post(request, *args, **kwargs)
 
 
 class CustomLogoutView(View):
