21a0afb9014342bd3cb63fea109fefc1ff8227e2
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-07-08 23:53:38 +0300

    fix20 text

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -2,9 +2,22 @@ import django_filters
 from django import forms
 
 from .models import Task
+from task_manager.labels.models import Label
 
 
 class TaskFilter(django_filters.FilterSet):
+    status = django_filters.ModelChoiceFilter(
+        queryset=Task._meta.get_field("status").related_model.objects.all(),
+        label="Статус"
+    )
+    executor = django_filters.ModelChoiceFilter(
+        queryset=Task._meta.get_field("executor").related_model.objects.all(),
+        label="Исполнитель"
+    )
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        label="Метка"
+    )
     self_tasks = django_filters.BooleanFilter(
         method="filter_self_tasks",
         label="Только свои задачи",
