fd4823b2df1376693628f8948b616bbb0d26383c
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-07-12 00:41:48 +0300

    fix27 final

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -4,18 +4,17 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
+#  class SelfOnlyMixin:
+#      """Разрешает действие только над собственным объектом."""
+#      permission_denied_url = reverse_lazy('users:list')
 
-class SelfOnlyMixin:
-    """Разрешает действие только над собственным объектом."""
-    permission_denied_url = reverse_lazy('users:list')
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user != self.get_object():
-            messages.error(request,
-                _("У вас нет прав для удаления другого пользователя.")
-            )
-            return redirect(self.permission_denied_url)
-        return super().dispatch(request, *args, **kwargs)
+#      def dispatch(self, request, *args, **kwargs):
+#          if request.user != self.get_object():
+#              messages.error(request,
+#                  _("У вас нет прав для удаления другого пользователя.")
+#              )
+#              return redirect(self.permission_denied_url)
+#          return super().dispatch(request, *args, **kwargs)
 
 
 class ProtectedCheckMixin:
